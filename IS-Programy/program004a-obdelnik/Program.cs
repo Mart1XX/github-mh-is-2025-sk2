dd2415900514089bcd29e924294461ec51bf7880
Martin Hruška <martashruska@gmail.com>
2025-11-05 11:26:33 +0100

    #7 Vytvoření programu na vykreslení obdelníku

--- IS-Programy/program004a-obdelnik/Program.cs
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Zobrazení obdelníku ************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************ Martin Hruška *****************");
+    Console.WriteLine("************** 23.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte šířku obdelníka: ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo pro šířku obdelníka znovu: ");
+    }
+    Console.Write("Zadejte výšku obdelníka: ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo pro výšku obdelníka znovu: ");
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
